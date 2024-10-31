e6af5f942426d858f7277ab33bc685939f159a67
Ana Julia Demattei <anajuliademattei26@gmail.com>
2024-10-31 15:54:11 -0300

    feat: create post user method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -7,7 +7,7 @@ const usersList = new UsersRepository();
 
 usuariosRoutes.get("/", (req, res) => {
   const usuarios = usersList.getAllUsers();
-  
+
   return res.status(200).json({
     message:
       usuarios.length == 0
@@ -16,5 +16,16 @@ usuariosRoutes.get("/", (req, res) => {
     usuarios,
   });
 });
+
+usuariosRoutes.post("/", (req,res) => {
+  const {name, email, password } = req.body
+
+  const user = usersList.addUser(name, email, password)
+
+  return res.status(201).json({
+    message: "Usuário cadastrado com sucesso!",
+    user,
+  })
+})
 
 export default usuariosRoutes;
